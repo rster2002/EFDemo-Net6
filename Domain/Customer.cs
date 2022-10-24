839baa137557b4c9a9b35827afedf9e5b5138364
rster2002 <rster2002dev@gmail.com>
2022-10-24 11:52:20 +0200

    🎉 Initial commit for .Net 6

---
+++ Domain/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+
+        [MaxLength(60)]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string Nickname { get; set; }
+
+        [NotMapped]
+        public int Age { get; set; }
+        public virtual ICollection<Invoice> Invoice { get; set; }
+    }
+}
---
+++ Domain/Invoice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Invoice
+    {
+        public int InvoiceID { get; set; }
+
+        public int CustomerID { get; set; }
+
+        public DateTime InvoiceDate { get; set; } 
+        public virtual Customer customer { get; set; }
+
+        public virtual ICollection<InvoiceLine> InvoiceLines { get; set; }
+    }
+}
